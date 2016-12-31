15e4b847e007772236622040629de90ec83b4084
xabixab <xabixab@gmail.com>
2016-12-31 13:37:02 +0100

    getDistanceBetweenPoints function

--- static/map_rect.js
+++ static/map_rect.js
@@ -5,16 +5,24 @@ $(function(){
 function initRect(){
   operationInProgress = true;
   console.log("Init Rect")
-  var slope = round(Math.tan((90 - pos.rot)*Math.PI/180), 8);
+  var slope = round(Math.tan((pos.rot)*Math.PI/180), 8);
   ctx.beginPath();
   var carCanvasPos = fromUnitsToCanvas(pos.x, pos.y);
   console.log(slope);
-  ctx.moveTo(carCanvasPos.x, carCanvasPos.y);
-  ctx.lineTo(slope*cy - carCanvasPos.x, cy);
+  y1 = slope * (-cx - carCanvasPos.x) + carCanvasPos.y
+  y2 = slope * (cx - carCanvasPos.x) + carCanvasPos.y
+  ctx.moveTo(-cx, y1);
+  ctx.lineTo( cx, y2);
 
-  ctx.moveTo(carCanvasPos.x, carCanvasPos.y);
-  ctx.lineTo(slope * cy * -1 - carCanvasPos.x, -cy);
   ctx.strokeStyle = "#00F";
   ctx.stroke();
+}
 
+function getDistanceBetweenPoints(p1, p2){
+  var xd = p2.x - p1.x;
+  var yd = p2.y - p1.y;
+  return Math.abs(Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2)));
+}
+
+function drawLine(){
 }
